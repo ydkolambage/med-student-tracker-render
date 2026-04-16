1b5828f878a9ee1fabea25e7240f2526ede71417
ydkolambage <yasas@med.sab.ac.lk>
2026-04-16 16:43:58 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py check
