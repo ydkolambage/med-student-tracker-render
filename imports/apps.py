dcf78ece3ac440a2e76e88cc7ddef5eb64e2a713
ydkolambage <yasas@med.sab.ac.lk>
2026-04-16 16:40:53 +0530

    Delete imports directory

--- imports/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImportsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "imports"
-    verbose_name = "Result Imports"
