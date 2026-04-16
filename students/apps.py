19a6e4ebce0a028722f2cf13b59046cc580a9123
ydkolambage <yasas@med.sab.ac.lk>
2026-04-16 16:41:32 +0530

    Delete students directory

--- students/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "students"
-    verbose_name = "Student Records"
