0459aa0823645bb9157f1bb6aebfeace0ed21466
ydkolambage <yasas@med.sab.ac.lk>
2026-04-16 16:41:07 +0530

    Delete results directory

--- results/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResultsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "results"
-    verbose_name = "Assessment Results"
